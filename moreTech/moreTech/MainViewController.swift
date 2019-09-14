05a36afb52d807b6507361ec4bb3f1bb9b32f878
Yoav05 <yoav.5.nem@gmail.com>
2019-09-14 16:48:27 +0300

    Recreate project

---
+++ moreTech/moreTech/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  moreTech
+//
+//  Created by Yoav on 14/09/2019.
+//  Copyright © 2019 Yoav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
