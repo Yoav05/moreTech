8bcb8737db137d87aaae600379a9a12df6073821
TheEshka <ya.the.eska@gmail.com>
2019-09-15 10:00:34 +0300

    add getParticipants

--- moreTech/moreTech/Networking/Manager/NetworkManager.swift
+++ moreTech/moreTech/Networking/Manager/NetworkManager.swift
@@ -102,11 +102,36 @@ struct NetworkManager {
                         completion ("Bad response")
                         return
                     }
-                    let apiResponse = try JSONDecoder().decode(EventResponse.self, from: responseData)
+                    _ = try JSONDecoder().decode(EventResponse.self, from: responseData)
                     completion(nil)
                 }catch{}
             }
         }
     }
+    
+    
+    func getParticipants(ownerId: String, meetingId: Int, completion: @escaping (_ participants: [ParticipantResponse]?,_ error: String?)->()){
+        router.request(.getParticipants(ownerId: ownerId, meetingId: meetingId)) { data, response, error in
+            if error != nil {
+                completion(nil, "Please check your network connection.")
+            }
+            if let response = response as? HTTPURLResponse{
+                if response.statusCode != 200 {
+                    completion (nil, "Bad request")
+                }
+                do{
+                    guard let responseData = data else {
+                        completion (nil, "Bad response")
+                        return
+                    }
+                    let apiResponse = try JSONDecoder().decode([ParticipantResponse].self, from: responseData)
+                    completion(apiResponse, nil)
+                }catch{}
+            }
+        }
+    }
+    
+    
+    
     
 }
